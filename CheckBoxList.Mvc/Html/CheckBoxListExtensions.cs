c29ec0c2ae7764404ff6329c2fe6a04dd7f6d035
stephen@complii.com.au <stephen@complii.com.au>
2016-03-19 21:04:21 +0800

    Code cleanup

--- CheckBoxList.Mvc/Html/CheckBoxListExtensions.cs
+++ CheckBoxList.Mvc/Html/CheckBoxListExtensions.cs
@@ -13,17 +13,17 @@ namespace CheckBoxList.Mvc.Html
     {
         public static MvcHtmlString CheckBoxList(this HtmlHelper htmlHelper, string name, IEnumerable<CheckBoxListItem> checkboxList)
         {
-            return CheckBoxListHelper(htmlHelper, null, name, checkboxList, null);
+            return CheckBoxListHelper(htmlHelper, name, checkboxList, null);
         }
 
         public static MvcHtmlString CheckBoxList(this HtmlHelper htmlHelper, string name, IEnumerable<CheckBoxListItem> checkboxList, object htmlAttributes)
         {
-            return CheckBoxListHelper(htmlHelper, null, name, checkboxList, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return CheckBoxListHelper(htmlHelper, name, checkboxList, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
         public static MvcHtmlString CheckBoxList(this HtmlHelper htmlHelper, string name, IEnumerable<CheckBoxListItem> checkboxList, IDictionary<string, object> htmlAttributes)
         {
-            return CheckBoxListHelper(htmlHelper, null, name, checkboxList, htmlAttributes);
+            return CheckBoxListHelper(htmlHelper, name, checkboxList, htmlAttributes);
         }
 
         public static MvcHtmlString CheckBoxListFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
@@ -44,57 +44,27 @@ namespace CheckBoxList.Mvc.Html
             if (expression == null)
                 throw new ArgumentNullException("expression");
 
-            var metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
             var name = ExpressionHelper.GetExpressionText(expression);
 
             var func = expression.Compile();
             var checkboxList = func(htmlHelper.ViewData.Model) as IEnumerable<CheckBoxListItem>;
 
-            return CheckBoxListHelper(htmlHelper, metadata, name, checkboxList, htmlAttributes);
+            return CheckBoxListHelper(htmlHelper, name, checkboxList, htmlAttributes);
         }
 
-        private static MvcHtmlString CheckBoxListHelper(HtmlHelper htmlHelper, ModelMetadata metadata, string expression,
-            IEnumerable<CheckBoxListItem> checkboxList, IDictionary<string, object> htmlAttributes)
+        private static MvcHtmlString CheckBoxListHelper(HtmlHelper htmlHelper, string expression, IEnumerable<CheckBoxListItem> checkboxList, IDictionary<string, object> htmlAttributes)
         {
-            return SelectInternal(htmlHelper, metadata, expression, checkboxList, htmlAttributes: htmlAttributes);
+            return SelectInternal(htmlHelper, expression, checkboxList, htmlAttributes);
         }
 
-        private static MvcHtmlString SelectInternal(this HtmlHelper htmlHelper, ModelMetadata metadata,
-            string name, IEnumerable<CheckBoxListItem> checkboxList, IDictionary<string, object> htmlAttributes)
+        private static MvcHtmlString SelectInternal(this HtmlHelper htmlHelper, string name, IEnumerable<CheckBoxListItem> checkboxList, IDictionary<string, object> htmlAttributes)
         {
             var fullName = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(name);
             if (string.IsNullOrEmpty(fullName))
             {
                 throw new ArgumentException("name");
             }
-
-            var usedViewData = false;
-
-            if (checkboxList == null)
-            {
-                checkboxList = htmlHelper.GetSelectData(name);
-                usedViewData = true;
-            }
-
-            var defaultValues = htmlHelper.GetModelStateValue(fullName, typeof(string[])) as IEnumerable<string>;
-
-            if (defaultValues == null && !string.IsNullOrEmpty(name))
-            {
-                if (!usedViewData)
-                {
-                    defaultValues = htmlHelper.ViewData.Eval(name) as IEnumerable<string>;
-                }
-                else if (metadata != null)
-                {
-                    defaultValues = metadata.Model as IEnumerable<string>;
-                }
-            }
-
-            if (defaultValues != null)
-            {
-                checkboxList = GetCheckBoxListWithDefaultValue(checkboxList, defaultValues);
-            }
-
+            
             var listItemBuilder = BuildItems(htmlHelper, name, checkboxList.ToList());
 
             var tagBuilder = new TagBuilder("div")
@@ -107,56 +77,7 @@ namespace CheckBoxList.Mvc.Html
 
             return new MvcHtmlString(tagBuilder.ToString(TagRenderMode.Normal));
         }
-
-        private static IEnumerable<CheckBoxListItem> GetSelectData(this HtmlHelper htmlHelper, string name)
-        {
-            object o = null;
-
-            if (htmlHelper.ViewData != null)
-            {
-                o = htmlHelper.ViewData.Eval(name);
-            }
-            if (o == null)
-            {
-                throw new InvalidOperationException("IEnumerable<CheckBoxListItem>");
-            }
-
-            var checkboxList = o as IEnumerable<CheckBoxListItem>;
-            if (checkboxList == null)
-            {
-                throw new InvalidOperationException("IEnumerable<CheckBoxListItem>");
-            }
-            return checkboxList;
-        }
-
-        private static object GetModelStateValue(this HtmlHelper htmlHelper, string key, Type destinationType)
-        {
-            ModelState modelState;
-
-            if (htmlHelper.ViewData.ModelState.TryGetValue(key, out modelState))
-            {
-                if (modelState.Value != null)
-                {
-                    return modelState.Value.ConvertTo(destinationType, null /* culture */);
-                }
-            }
-            return null;
-        }
-
-        private static IEnumerable<CheckBoxListItem> GetCheckBoxListWithDefaultValue(IEnumerable<CheckBoxListItem> checkboxList, IEnumerable<string> defaultValues)
-        {
-            var selectedValues = new HashSet<string>(defaultValues, StringComparer.OrdinalIgnoreCase);
-            var newCheckBoxList = new List<CheckBoxListItem>();
-
-            foreach (var item in checkboxList)
-            {
-                item.IsChecked = (item.Value != null) ? selectedValues.Contains(item.Value) : selectedValues.Contains(item.Text);
-                newCheckBoxList.Add(item);
-            }
-
-            return newCheckBoxList;
-        }
-
+        
         private static StringBuilder BuildItems(this HtmlHelper htmlHelper, string name, IList<CheckBoxListItem> checkboxList)
         {
             var listItemBuilder = new StringBuilder();
